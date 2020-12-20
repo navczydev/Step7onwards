95f0ce53ed06d2b0c2030d441ab155fa7f048633
Nav Singh <navsingh191@gmail.com>
2020-12-19 21:04:44 -0500

    prope changes

---
+++ lib/unit_converter.dart
@@ -0,0 +1,302 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+import 'category.dart';
+import 'unit.dart';
+
+const _padding = EdgeInsets.all(16.0);
+
+/// [UnitConverter] where users can input amounts to convert in one [Unit]
+/// and retrieve the conversion in another [Unit] for a specific [Category].
+class UnitConverter extends StatefulWidget {
+  /// The current [Category] for unit conversion.
+  final Category category;
+
+  /// This [UnitConverter] takes in a [Category] with [Units]. It can't be null.
+  const UnitConverter({
+    @required this.category,
+  }) : assert(category != null);
+
+  @override
+  _UnitConverterState createState() => _UnitConverterState();
+}
+
+class _UnitConverterState extends State<UnitConverter> {
+  Unit _fromValue;
+  Unit _toValue;
+  double _inputValue;
+  String _convertedValue = '';
+  List<DropdownMenuItem> _unitMenuItems;
+  bool _showValidationError = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _createDropdownMenuItems();
+    _setDefaults();
+  }
+
+  @override
+  void didUpdateWidget(UnitConverter old) {
+    super.didUpdateWidget(old);
+    // We update our [DropdownMenuItem] units when we switch [Categories].
+    if (old.category != widget.category) {
+      _createDropdownMenuItems();
+      _setDefaults();
+    }
+  }
+
+  /// Creates fresh list of [DropdownMenuItem] widgets, given a list of [Unit]s.
+  void _createDropdownMenuItems() {
+    var newItems = <DropdownMenuItem>[];
+    for (var unit in widget.category.units) {
+      newItems.add(DropdownMenuItem(
+        value: unit.name,
+        child: Container(
+          child: Text(
+            unit.name,
+            style: TextStyle(color: Colors.lightBlue),
+            softWrap: true,
+          ),
+        ),
+      ));
+    }
+    setState(() {
+      _unitMenuItems = newItems;
+    });
+  }
+
+  /// Sets the default values for the 'from' and 'to' [Dropdown]s, and the
+  /// updated output value if a user had previously entered an input.
+  void _setDefaults() {
+    setState(() {
+      _fromValue = widget.category.units[0];
+      _toValue = widget.category.units[1];
+    });
+  }
+
+  /// Clean up conversion; trim trailing zeros, e.g. 5.500 -> 5.5, 10.0 -> 10
+  String _format(double conversion) {
+    var outputNum = conversion.toStringAsPrecision(7);
+    if (outputNum.contains('.') && outputNum.endsWith('0')) {
+      var i = outputNum.length - 1;
+      while (outputNum[i] == '0') {
+        i -= 1;
+      }
+      outputNum = outputNum.substring(0, i + 1);
+    }
+    if (outputNum.endsWith('.')) {
+      return outputNum.substring(0, outputNum.length - 1);
+    }
+    return outputNum;
+  }
+
+  void _updateConversion() {
+    setState(() {
+      _convertedValue =
+          _format(_inputValue * (_toValue.conversion / _fromValue.conversion));
+    });
+  }
+
+  void _updateInputValue(String input) {
+    setState(() {
+      if (input == null || input.isEmpty) {
+        _convertedValue = '';
+      } else {
+        // Even though we are using the numerical keyboard, we still have to check
+        // for non-numerical input such as '5..0' or '6 -3'
+        try {
+          final inputDouble = double.parse(input);
+          _showValidationError = false;
+          _inputValue = inputDouble;
+          _updateConversion();
+        } on Exception catch (e) {
+          print('Error: $e');
+          _showValidationError = true;
+        }
+      }
+    });
+  }
+
+  Unit _getUnit(String unitName) {
+    return widget.category.units.firstWhere(
+      (Unit unit) {
+        return unit.name == unitName;
+      },
+      orElse: null,
+    );
+  }
+
+  void _updateFromConversion(dynamic unitName) {
+    setState(() {
+      _fromValue = _getUnit(unitName);
+    });
+    if (_inputValue != null) {
+      _updateConversion();
+    }
+  }
+
+  void _updateToConversion(dynamic unitName) {
+    setState(() {
+      _toValue = _getUnit(unitName);
+    });
+    if (_inputValue != null) {
+      _updateConversion();
+    }
+  }
+
+  Widget _createDropdown(String currentValue, ValueChanged<dynamic> onChanged) {
+    return Container(
+      margin: EdgeInsets.only(top: 16.0),
+      decoration: BoxDecoration(
+        // This sets the color of the [DropdownButton] itself
+        color: Colors.grey[50],
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        border: Border.all(
+            color: Colors.lightBlue, width: 2.0, style: BorderStyle.solid),
+      ),
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Theme(
+        // This sets the color of the [DropdownMenuItem]
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.grey[50],
+        ),
+        child: DropdownButtonHideUnderline(
+          child: ButtonTheme(
+            alignedDropdown: true,
+            child: DropdownButton(
+              value: currentValue,
+              items: _unitMenuItems,
+              onChanged: onChanged,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final input = Padding(
+      padding: _padding,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // This is the widget that accepts text input. In this case, it
+          // accepts numbers and calls the onChanged property on update.
+          // You can read more about it here: https://flutter.io/text-input
+          TextField(
+            style: Theme.of(context)
+                .textTheme
+                .headline4
+                .copyWith(color: Colors.lightBlue),
+            decoration: InputDecoration(
+              labelStyle: Theme.of(context)
+                  .textTheme
+                  .headline4
+                  .copyWith(color: Colors.lightBlue),
+              errorText: _showValidationError ? 'Invalid number entered' : null,
+              labelText: 'Input',
+              enabledBorder: OutlineInputBorder(
+                // width: 0.0 produces a thin "hairline" border
+                borderSide:
+                    const BorderSide(color: Colors.lightBlue, width: 2.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                // width: 0.0 produces a thin "hairline" border
+                borderSide:
+                    const BorderSide(color: Colors.lightBlue, width: 2.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.lightBlue),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+            // Since we only want numerical input, we use a number keyboard. There
+            // are also other keyboards for dates, emails, phone numbers, etc.
+            keyboardType: TextInputType.number,
+            onChanged: _updateInputValue,
+          ),
+          _createDropdown(_fromValue.name, _updateFromConversion),
+        ],
+      ),
+    );
+
+    final arrows = RotatedBox(
+      quarterTurns: 1,
+      child: Icon(
+        Icons.compare_arrows,
+        color: Colors.lightBlue,
+        size: 40.0,
+      ),
+    );
+
+    final output = Padding(
+      padding: _padding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          InputDecorator(
+            child: Text(
+              _convertedValue,
+              style: Theme.of(context)
+                  .textTheme
+                  .headline4
+                  .copyWith(color: Colors.lightBlue),
+            ),
+            decoration: InputDecoration(
+              labelText: 'Output',
+              labelStyle: Theme.of(context)
+                  .textTheme
+                  .headline4
+                  .copyWith(color: Colors.lightBlue),
+              enabledBorder: OutlineInputBorder(
+                // width: 0.0 produces a thin "hairline" border
+                borderSide:
+                    const BorderSide(color: Colors.lightBlue, width: 2.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+          ),
+          _createDropdown(_toValue.name, _updateToConversion),
+        ],
+      ),
+    );
+    final _widgets = [input, arrows, output];
+    final converter = Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: _widgets,
+    );
+
+    // DONE: Use an OrientationBuilder to add a width to the unit converter
+    return Padding(
+        padding: _padding,
+        // RenderFlex overflow issue fix
+        //child: SingleChildScrollView(child: converter));
+        child: OrientationBuilder(
+            builder: (BuildContext context, Orientation orientation) {
+          if (orientation == Orientation.portrait) {
+            return ListView.builder(
+              itemBuilder: (BuildContext context, int index) {
+                return _widgets[index];
+              },
+              itemCount: _widgets.length,
+            );
+          } else {
+            // in landscape mode
+            return SingleChildScrollView(
+                child:
+                    Center(child: Container(width: 400.0, child: converter)));
+          }
+        }));
+  }
+}
